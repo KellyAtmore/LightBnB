3b8330882cefc359e280cfe2a22d688ef814ec70
Kelly Atmore <kelly.atmore47@gmail.com>
2022-05-05 12:55:16 -0700

    add reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -78,17 +78,7 @@ const addUser =  function(user) {
     .catch((err) => {
       console.log(err.message);
     });
-  
-  
-  
-  
-  
-  
-  // const userId = Object.keys(users).length + 1;
-  // user.id = userId;
-  // users[userId] = user;
-  // return Promise.resolve(user);
-}
+};
 exports.addUser = addUser;
 
 /// Reservations
@@ -99,7 +89,33 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  
+  const values = [guest_id, limit];
+  
+  return pool.query(`
+  SELECT properties.*, reservations.*, avg(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date desc
+    LIMIT $2;
+  `, values)
+    .then((result) => {
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+  
+  
+  
+  
+  
+  
+  
+  //return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
